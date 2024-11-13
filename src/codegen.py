a6cf9526aa14f62d9839b922fa6cdd7810cc0ad3
rej <rzioma@acm.org>
2024-11-13 13:38:53 +0100

    Senary weight support

--- src/codegen.py
+++ src/codegen.py
@@ -45,18 +45,15 @@ assert find_max_size(all_combinations(ternary, 5), pack_ternary_weights) <= 3**5
 assert check_if_all_unique(all_combinations(ternary, 5), pack_ternary_weights) == True
 assert check_if_all_match(all_combinations(ternary, 5),  pack_ternary_weights, unpack_ternary_weights) == True
 
-# septenery_mapping = {-2:6, -1:5, -.5:4, 0:0, .5:1, 1:2, 2:3} # 7 states
-# quinary_mapping_a = {-2:4, -1:3,        0:0,       1:1, 2:2} # 5 states
-# quinary_mapping_b = {      -1:4, -.5:3, 0:0, .5:1, 1:2     } # 5 states
-
 def find_closest_index(arr, target):
     return min(range(len(arr)), key=lambda i: abs(arr[i] - target))
 
-septenery_mapping = [0, .5, 1, 2, -.5, -1, -2] # 7 positions
-quinary_mapping_a = [0,     1, 2,      -1, -2] # 5 positions
-quinary_mapping_b = [0, .5, 1, 2, -.5, -1    ] # 5 positions
-# quinary_mapping_a = {-2:4, -1:3,        0:0,       1:1, 2:2} # 5 states
-# quinary_mapping_b = {      -1:4, -.5:3, 0:0, .5:1, 1:2     } # 5 states
+septenery_mapping = [0, .5, 1, 2, -.5, -1, -2] # 7 possible states
+senary_mapping_a  = [0, .5, 1, 2,      -1, -2] # 6 possible states
+senary_mapping_b  = [0,     1, 2, -.5, -1, -2] # 6 possible states
+quinary_mapping_a = [0,     1, 2,      -1, -2] # 5 possible states
+quinary_mapping_b = [0, .5, 1,    -.5, -1    ] # 5 possible states
+ternary_mapping   = [0,     1,         -1    ] # 3 possible states
 
 def pack_775_weights(weights, quinary_mapping = quinary_mapping_a):
     packed = 0
@@ -65,7 +62,7 @@ def pack_775_weights(weights, quinary_mapping = quinary_mapping_a):
         packed = (packed * n) + find_closest_index(mapping, w)
     return packed
 
-def unpack_775_weights(packed, number_of_weights=3, quinary_mapping = quinary_mapping_a):
+def unpack_775_weights(packed, number_of_weights=3, quinary_mapping=quinary_mapping_a):
     weights = []
     # Inverting the dictionary
     for _, n in zip(range(number_of_weights), itertools.cycle(reversed([7,7,5]))):
@@ -74,6 +71,25 @@ def unpack_775_weights(packed, number_of_weights=3, quinary_mapping = quinary_ma
         packed //= n
     return weights[::-1]
 
+def pack_676_weights(weights):
+    packed = 0
+    for w, n, mapping in zip(weights,
+                            itertools.cycle([6,7,6]),
+                            itertools.cycle([senary_mapping_a,septenery_mapping,senary_mapping_a])):
+        packed = (packed * n) + find_closest_index(mapping, w)
+    return packed
+
+def unpack_676_weights(packed, number_of_weights=3, senary_mapping_1=senary_mapping_a, senary_mapping_2=senary_mapping_a):
+    weights = []
+    # Inverting the dictionary
+    for _, n, mapping in zip(range(number_of_weights),
+                            itertools.cycle(reversed([6,7,6])),
+                            itertools.cycle([senary_mapping_1,septenery_mapping,senary_mapping_2])):
+        weights.append(mapping[packed % n])
+        packed //= n
+    return weights[::-1]
+
+# test 775
 quinary   = [-2,-1,    0,   1,2]
 septenery = [-2,-1,-.5,0,.5,1,2]
 assert find_max_size(all_combinations(septenery, 3), pack_775_weights) <= 255
@@ -82,6 +98,15 @@ assert check_if_all_unique(all_combinations(septenery, 3), pack_775_weights) == 
 assert check_if_all_match(all_combinations(quinary, 3), pack_775_weights, unpack_775_weights) == True
 assert check_if_all_match(itertools.product(septenery, septenery, quinary), pack_775_weights, unpack_775_weights) == True
 
+# test 676
+senary_1 = [-2,-1,   0,.5,1,2]
+# senary_2 = [-2,-1,-5,0   ,1,2] # @TODO: fix balanced senary with a pair of alternating encodings
+senary_2 = senary_1
+assert find_max_size(all_combinations(septenery, 3), pack_676_weights) <= 255
+assert find_max_size(all_combinations(septenery, 3), pack_676_weights) == 6*7*6
+assert check_if_all_unique(all_combinations(septenery, 3), pack_676_weights) == True
+assert check_if_all_match(all_combinations(quinary, 3), pack_676_weights, unpack_676_weights) == True
+assert check_if_all_match(itertools.product(senary_1, septenery, senary_2), pack_676_weights, unpack_676_weights) == True
 
 
 def generate_verilog_module_to_unpack_ternary_weights(reverse_weight_order = True):
@@ -109,6 +134,44 @@ def generate_verilog_module_to_unpack_ternary_weights(reverse_weight_order = Tru
             f" end // {pretty_printed}")
     print(\
         "        default: {zero, sign} = {5'b11_111, 5'b0}; // Default case\n"
+        "        endcase\n"
+        "    end\n"
+        "endmodule\n")
+
+def generate_verilog_module_to_unpack_676_weights(reverse_weight_order = True):
+    print(\
+        "module unpack_676_weights(input      [7:0] packed_weights,\n"
+        "                          output reg [2:0] zero,\n"
+        "                          output reg [2:0] sign,\n"
+        "                          output reg [2:0] mul2,\n"
+        "                          output reg [2:0] div2);\n"
+        "    always @(*) begin\n"
+        "        case(packed_weights)")
+
+    unpack_weights = unpack_676_weights
+    for i in range(6*7*6):
+        zero = 0b111 # if i != 0 else 1
+        sign = 0
+        mul2 = 0
+        div2 = 0
+        unpacked = unpack_weights(i)
+        for w in (reversed(unpacked) if reverse_weight_order else unpacked):
+            zero = (zero << 1) | (w == 0)
+            sign = (sign << 1) | (w  < 0)
+            mul2 = (mul2 << 1) | (abs(w) ==  2)
+            div2 = (div2 << 1) | (abs(w) == .5)
+        zero &= 0b111
+
+        pretty_printed = ''.join([f"{str(n):>5}" \
+            for n in (reversed(unpacked) if reverse_weight_order else unpacked)])
+        print(f"        8'd{str(i).zfill(3)}: begin"
+            f" zero = 3'b{bin(zero)[2:].zfill(3)};"
+            f" sign = 3'b{bin(sign)[2:].zfill(3)};"
+            f" mul2 = 3'b{bin(mul2)[2:].zfill(3)};"
+            f" div2 = 3'b{bin(div2)[2:].zfill(3)};"
+            f" end // {pretty_printed}")
+    print(\
+        "        default: {zero, sign, mul2, div2} = {3'b111, 3'b0, 3'b0, 3'b0}; // Default case\n"
         "        endcase\n"
         "    end\n"
         "endmodule\n")
@@ -152,5 +215,6 @@ def generate_verilog_module_to_unpack_775_weights(reverse_weight_order = True):
         "endmodule\n")
 
 
-#generate_verilog_module_to_unpack_ternary_weights()
+generate_verilog_module_to_unpack_ternary_weights()
+generate_verilog_module_to_unpack_676_weights()
 generate_verilog_module_to_unpack_775_weights()
